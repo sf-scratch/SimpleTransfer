43e8db1b6daf7ae139e7e90047d31e6969221775
雷昊 <1763271443@qq.com>
2024-12-19 20:18:26 +0800

    添加项目文件。

---
+++ App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Prism.DryIoc;
+using Prism.Ioc;
+using SimpleTransfer.ViewModels;
+using SimpleTransfer.Views;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SimpleTransfer
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<Main>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<Main, MainViewModel>();
+        }
+    }
+}
