25326b0e438be30557debf92c2625658315cd2df
雷昊 <1763271443@qq.com>
2024-12-19 22:00:20 +0800

    简单界面

---
+++ Converters/EventArgsConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows;
+
+namespace SimpleTransfer.Converters
+{
+    public class DragEventArgsConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return new Tuple<object, DragEventArgs>(parameter, value as DragEventArgs);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -1,13 +1,70 @@
-﻿using Prism.Mvvm;
+﻿using ImTools;
+using Prism.Commands;
+using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace SimpleTransfer.ViewModels
 {
-    internal class MainViewModel : BindableBase
+    public class MainViewModel : BindableBase
     {
+        public DelegateCommand<Tuple<object, DragEventArgs>> DragOverCommand { get; set; }
+        public DelegateCommand<Tuple<object, DragEventArgs>> DropCommand { get; set; }
+        public MainViewModel()
+        {
+            DragOverCommand = new DelegateCommand<Tuple<object, DragEventArgs>>(DragOver);
+            DropCommand = new DelegateCommand<Tuple<object, DragEventArgs>>(Drop);
+        }
+
+        private void Drop(Tuple<object, DragEventArgs> tuple)
+        {
+            DragEventArgs eventArgs = tuple.Item2;
+            if (eventArgs == null)
+            {
+                return;
+            }
+            var files = (string[])eventArgs.Data.GetData(DataFormats.FileDrop);
+            if (files == null || HasDirectory(files))
+                return;
+            MessageBox.Show(string.Join("\r", files));
+        }
+
+        private void DragOver(Tuple<object, DragEventArgs> tuple)
+        {
+            DragEventArgs eventArgs = tuple.Item2;
+            if (eventArgs == null)
+            {
+                return;
+            }
+            var files = (string[])eventArgs.Data.GetData(DataFormats.FileDrop);
+            if (HasDirectory(files))
+            {
+                eventArgs.Effects = DragDropEffects.None;// 抛弃这一次的拖拽源
+                eventArgs.Handled = true;
+            }
+        }
+
+        /// <summary>
+        /// 包含目录文件判断
+        /// </summary>
+        /// <param name="files"></param>
+        /// <returns></returns>
+        private bool HasDirectory(string[] files)
+        {
+            if (files == null)
+                return false;
+            for (int i = 0; i < files.Length; i++)
+            {
+                if (System.IO.Directory.Exists(files[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 }
